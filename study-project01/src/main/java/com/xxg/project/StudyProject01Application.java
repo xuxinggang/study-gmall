9b9c810f5fabf056532a9ce367ff382612f52878
xxg_glb <welcome_glb@qq.com>
2020-02-26 22:41:52 +0800

    提交测试

---
+++ study-project01/src/main/java/com/xxg/project/StudyProject01Application.java
@@ -0,0 +1,13 @@
+package com.xxg.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyProject01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyProject01Application.class, args);
+    }
+
+}
---
+++ study-project01/src/test/java/com/xxg/project/StudyProject01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xxg.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyProject01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
